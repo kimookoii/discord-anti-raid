61851377c75ed29ce7c0fcd0aeab0f35007ef210
Glitch (brindle-succulent-soccer) <none>
2021-05-31 09:58:34 +0000

    🌅📷 Checkpoint ./commands/main/serverlist.js:22439621/2238

--- commands/main/serverlist.js
+++ commands/main/serverlist.js
@@ -19,13 +19,14 @@ module.exports = {
           .map(r => r)
           .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`)
           .slice(0, 10)
-          .join("\n");
+          .join("\n\n");
 
         let embed = new Discord.MessageEmbed()
-          .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-          .setColor("GREEN")
-          .setFooter(client.user.username)
-          .setTitle(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+          .setAuthor(`SERVER LIST`)
+          .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+          .setFooter(`${dev} • Halaman - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+          .setColor(color)
+          .setTimestamp()
           .setDescription(description);
 
         let msg = await message.channel.send(embed);
@@ -43,79 +44,56 @@ module.exports = {
           i1 = i1 - 10;
           page = page - 1;
 
-          // if there is no guild to display, delete the message
-          if (i0 + 1 < 0) {
+        if (i0 + 1 < 0) {
             console.log(i0)
-            return msg.delete();
-          }
-          if (!i0 || !i1) {
-            return msg.delete();
-          }
+            return msg.delete(); }
+          
+        if (!i0 || !i1) {
+            return msg.delete(); }
 
-          description =
-            `Total Servers - ${client.guilds.cache.size}\n\n` +
-            client.guilds.cache
-              .sort((a, b) => b.memberCount - a.memberCount)
-              .map(r => r)
-              .map(
-                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members`
-              )
-              .slice(i0, i1)
-              .join("\n");
+        description = `Total Servers - ${client.guilds.cache.size}\n\n` +
+        client.guilds.cache
+          .sort((a, b) => b.memberCount - a.memberCount)
+          .map(r => r)
+          .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members`)
+          .slice(i0, i1)
+          .join("\n");
 
-          // Update the embed with new informations
-          embed
-            .setTitle(
-              `Page - ${page}/${Math.round(client.guilds.cache.size / 10 + 1)}`
-            )
-            .setDescription(description);
+        embed.setTitle(`Page - ${page}/${Math.round(client.guilds.cache.size / 10 + 1)}`)
+             .setDescription(description);
 
-          // Edit the message
-          msg.edit(embed);
-        }
+        msg.edit(embed);}
 
         if (reaction._emoji.name === "➡") {
-          // Updates variables
+          
           i0 = i0 + 10;
           i1 = i1 + 10;
           page = page + 1;
 
-          // if there is no guild to display, delete the message
-          if (i1 > client.guilds.cache.size + 10) {
-            return msg.delete();
-          }
-          if (!i0 || !i1) {
-            return msg.delete();
-          }
+        if (i1 > client.guilds.cache.size + 10) {
+            return msg.delete(); }
+        if (!i0 || !i1) {
+            return msg.delete(); }
 
-          description =
-            `Total Servers - ${client.guilds.cache.size}\n\n` +
-            client.guilds.cache
-              .sort((a, b) => b.memberCount - a.memberCount)
-              .map(r => r)
-              .map(
-                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members`
-              )
-              .slice(i0, i1)
-              .join("\n");
+        description =`Total Servers - ${client.guilds.cache.size}\n\n` +
+        client.guilds.cache
+          .sort((a, b) => b.memberCount - a.memberCount)
+          .map(r => r)
+          .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members`)
+          .slice(i0, i1)
+          .join("\n");
 
-          // Update the embed with new informations
-          embed
-            .setTitle(
-              `Page - ${page}/${Math.round(client.guilds.cache.size / 10 + 1)}`
-            )
-            .setDescription(description);
+        embed.setTitle(`Page - ${page}/${Math.round(client.guilds.cache.size / 10 + 1)}`)
+             .setDescription(description);
 
-          // Edit the message
-          msg.edit(embed);
+        msg.edit(embed);
         }
 
         if (reaction._emoji.name === "❌") {
           return msg.delete();
         }
 
-        // Remove the reaction when the user react to the message
         await reaction.users.remove(message.author.id);
       });
     }
-}
+  }
